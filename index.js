7be6d60c63c77a2d8cde0b6c6efae1c38f6ae3c7
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-03-10 16:07:46 +0800

    Fix typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const genProjection = require('./src/projection');
-const genResolver = require('./src/resolvers');
+const genResolver = require('./src/resolver');
 
 const genResolvers = (configs) => _.mapValues(configs, genResolver);
 
